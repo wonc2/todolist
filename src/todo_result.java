3cbfb75d85a414d2c429886ad41f2755d00bab6f
keyy1315 <lovelina1315@gmail.com>
2024-08-07 17:25:40 +0900

    create todo_result_class

--- src/todo_result.java
+++ src/todo_result.java
@@ -1,2 +1,12 @@
+import java.util.List;
+import java.util.Map;
+
 public class todo_result {
+    Map<String, List<Integer>> result_map;
+    public todo_result(Map<String, List<Integer>> result_map) {
+        this.result_map = result_map;
+    }
+    public void todo_result_main() {
+
+    }
 }
